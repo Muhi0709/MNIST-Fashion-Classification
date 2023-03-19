f73bc18c2f1b4b6cdac72212a86ed96b55c4a613
Muhi0709 <62468783+Muhi0709@users.noreply.github.com>
2023-03-19 12:01:51 +0530

    Updated configurations for Mnist

--- mnist_test.py
+++ mnist_test.py
@@ -1,45 +1,116 @@
 from NN_model import nn_model
 from data_loader import get_mnist
-import wandb
 
-wandb.login()
+print("loading dataset")
 data=get_mnist()
+print("dataset loaded successfully")
 
-def train_test_mnist(config_defaults,seed,name):
-
-    wandb.init(project="cs6910_assignment_1",config=config_defaults,name=name)
-    config=wandb.config
+def train_test_mnist(config,seed):
     (X_train,y_train),(X_test,y_test)=data
     input_dimension = X_train[0].shape[0]
-    num_of_classes=config.num_of_classes
-    num_of_neurons_list = [config.hidden_size]*(config.num_layers) +[num_of_classes]
-    activation_func_list = ([config.activation]*(config.num_layers) + ["softmax"]) if config.activation!="leakyreLU" else ([(config.activation,config.leakyalpha)]*(config.num_layers) + ["softmax"])
+    num_of_classes=config["num_of_classes"]
+    num_of_neurons_list = [config["hidden_size"]]*(config["num_layers"]) +[num_of_classes]
+    activation_func_list = ([config["activation"]]*(config["num_layers"]) + ["softmax"]) if config["activation"]!="leakyreLU" else ([(config["activation"],config["leakyalpha"])]*(config["num_layers"]) + ["softmax"])
 
 
-    best_model=nn_model()
-    best_model.compact_build_nn(input_dimension,config.num_layers+1,num_of_neurons_list,activation_func_list,
-                       config.weight_init,seed)
-    
-    best_model.training(X_train,y_train,val_percent=config.val_percent,loss_func= config.loss,optimizer=config.optimizer,
-                  max_epoch=config.epochs,batch_size=config.batch_size,learning_rate=config.learning_rate,
-                  beta=config.momentum,rmsbeta=config.beta,beta_1=config.beta1,beta_2=config.beta2,epsilon=config.epsilon,L2_decay=config.weight_decay,
-                  early_stopping=config.early_stopping,patience=config.patience,data_augmentation=config.data_augmentation,seed=seed)
-    wandb.finish()
+    model=nn_model()
+    model.compact_build_nn(input_dimension,config["num_layers"]+1,num_of_neurons_list,activation_func_list,
+                       config["weight_init"],seed)
+    print("Test-{} Configuration_used:".format(seed+1))
+    for key,value in config.items():
+        print(key,":",value)
+    model.training(X_train,y_train,val_percent=config["val_percent"],loss_func= config["loss"],optimizer=config["optimizer"],
+                  max_epoch=config["epochs"],batch_size=config["batch_size"],learning_rate=config["learning_rate"],
+                  beta=config["momentum"],rmsbeta=config["beta"],beta_1=config["beta1"],beta_2=config["beta2"],epsilon=config["epsilon"],L2_decay=config["weight_decay"],
+                  early_stopping=config["early_stopping"],patience=config["patience"],data_augmentation=config["data_augmentation"],seed=seed,wandb_log=False)
 
-    acc,loss=best_model.compute_loss_accuracy(X_test,y_test)
-    print("-"*100)
-    print("=>Test Accuracy:",acc)
-    print("=>Test Loss:",loss)
+    y_test_encoded=model.one_hot_encoding(y_test)
+    acc,loss=model.compute_loss_accuracy(X_test,y_test_encoded,config["loss"])
+    print("=>{} Test Accuracy:".format(config["dataset"]),acc)
+    print("=>{} Test Loss:".format(config["dataset"]),loss)
+    print("-"*200)
 
-config_1={}
-config_2={}
-config_3={}
 
-print("Test-1")
-train_test_mnist(config_1,0,"test_1")
-print("Test-2")
-train_test_mnist(config_2,0,"test_2")
-print("Test-3",config_3,0,"test_3")
+config_1={
+    "dataset": "mnist",
+    "epochs": 15,
+    "batch_size": 64,
+    "num_layers": 5,
+    "hidden_size": 64,
+    "learning_rate": 2.5 * (10**-4),
+    "momentum": 0.9,
+    "beta": 0.9,
+    "beta1":0.99,
+    "beta2":0.999,
+    "epsilon":10**-8,
+    "optimizer": "nadam",
+    "weight_decay": 0.001,
+    "weight_init": "Xavier",
+    "activation": "leakyreLU",
+    "loss": "cross_entropy",
+    "leakyalpha":0.1,
+    "val_percent": 0.1,
+    "num_of_classes": 10,
+    "early_stopping": False,
+    "patience": 2,
+    "data_augmentation": False,       
+    }
+
+config_2={
+    "dataset": "mnist",
+    "epochs": 20,
+    "batch_size": 64,
+    "num_layers": 5,
+    "hidden_size": 64,
+    "learning_rate": 2.5 * (10**-4),
+    "momentum": 0.9,
+    "beta": 0.9,
+    "beta1":0.99,
+    "beta2":0.999,
+    "epsilon":10**-8,
+    "optimizer": "nadam",
+    "weight_decay": 0.001,
+    "weight_init": "Xavier",
+    "activation": "leakyreLU",
+    "loss": "cross_entropy",
+    "leakyalpha":0.1,
+    "val_percent": 0.1,
+    "num_of_classes": 10,
+    "early_stopping": True,
+    "patience": 3,
+    "data_augmentation": True,       
+    }
+config_3={
+    "dataset": "mnist",
+    "epochs": 20,
+    "batch_size": 64,
+    "num_layers": 5,
+    "hidden_size": 128,
+    "learning_rate": 7.5 * (10**-4),
+    "momentum": 0.9,
+    "beta": 0.9,
+    "beta1":0.99,
+    "beta2":0.999,
+    "epsilon":10**-8,
+    "optimizer": "adam",
+    "weight_decay": 0.001,
+    "weight_init": "Xavier",
+    "activation": "leakyreLU",
+    "loss": "cross_entropy",
+    "leakyalpha":0.1,
+    "val_percent": 0.1,
+    "num_of_classes": 10,
+    "early_stopping": True,
+    "patience": 3,
+    "data_augmentation": True,       
+    }
+
+seed1,seed2,seed3=0,1,2
+train_test_mnist(config_1,seed1)
+train_test_mnist(config_2,seed2)
+train_test_mnist(config_3,seed3)
+
+
 
 
 
